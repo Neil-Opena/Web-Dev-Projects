8b8cbfbdd7a168a4d963b831db6ab85333b51f19
Neil Opena <neil1114o@gmail.com>
2017-06-26 00:09:00 -0400

    configured sticky navigation (Omnifood)

--- OtherCourse/Omnifood/resources/js/script.js
+++ OtherCourse/Omnifood/resources/js/script.js
@@ -4,6 +4,20 @@ $(document).ready(function(){
 	// 	$(this).css('background-color', '#ff0000');
 	// })
 
+	// var waypoints = $('#handler-first').waypoint(function(direction) {
+	//   notify(this.element.id + ' hit 25% from top of window') 
+	// }, {
+	//   offset: '25%'
+	// })
 	
+	$('.js--section-features').waypoint(function(direction){
+		if(direction == "down"){
+			$('nav').addClass('sticky');
+		}else{
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset: '60px'
+	});
 
 });
