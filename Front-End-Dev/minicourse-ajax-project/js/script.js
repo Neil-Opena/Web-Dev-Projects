c88931799f0478d4f6b6c63a04ed6352a63386f3
Neil Opena <neil1114o@gmail.com>
2017-08-02 00:39:18 -0400

    Added NyTimes API

--- Front-End-Dev/minicourse-ajax-project/js/script.js
+++ Front-End-Dev/minicourse-ajax-project/js/script.js
@@ -20,6 +20,16 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + url +'">');
     // YOUR CODE GOES HERE!
 
+    $.getJSON("https://api.nytimes.com/svc/search/v2/articlesearch.json",{'api-key': "64274d8727394330bfd1e06b8f347b7e"},function(data){
+        var articles = [];
+        $.each(data.response.docs, function(){
+            articles.push($(this)[0]);
+        });
+        for(var i = 0; i < articles.length; i++){
+            console.log(articles[i]);
+            $nytElem.append("<li>" + articles[i].headline.main + "</li>");
+        }
+    });
     return false;
 };
 
