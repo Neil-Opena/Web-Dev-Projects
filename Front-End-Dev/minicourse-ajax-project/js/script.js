401c9d442948c166c269d8ede5c699071d1eafa5
Neil Opena <neil1114o@gmail.com>
2017-08-02 01:09:32 -0400

    Added some error handling

--- Front-End-Dev/minicourse-ajax-project/js/script.js
+++ Front-End-Dev/minicourse-ajax-project/js/script.js
@@ -20,16 +20,23 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + url +'">');
     // YOUR CODE GOES HERE!
 
-    $.getJSON("https://api.nytimes.com/svc/search/v2/articlesearch.json",{'api-key': "64274d8727394330bfd1e06b8f347b7e"},function(data){
+    $.getJSON("https://api.nytimes.com/svc/search/v2/articlesearch.json",{'api-key': "64274d8727394330bfd1e06b8f347b7e", 'q':address},function(data){
         var articles = [];
-        $.each(data.response.docs, function(){
-            articles.push($(this)[0]);
-        });
-        for(var i = 0; i < articles.length; i++){
-            console.log(articles[i]);
-            $nytElem.append("<li>" + articles[i].headline.main + "</li>");
-        }
-    });
+            $nytHeaderElem.text('New York Times Articles About ' + street);
+            for(var i = 0; i < 5; i++){
+                articles.push(data.response.docs[0])
+            }
+            for(var i = 0; i < articles.length; i++){
+                console.log(articles[i]);
+                var urlNYT = articles[i].web_url;
+                var headline = articles[i].headline.main;
+                var paragraph = articles[i].snippet;
+                $nytElem.append('<li class="article"> <a href="' + url + '">' + headline +"</a><p>" + paragraph + "</p></li>");
+            }
+
+    }).fail(function(){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });;
     return false;
 };
 
