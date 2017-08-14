3a07ce474b40136e6d98cfeb3d2f16d3cf0efa88
Neil Opena <neil1114o@gmail.com>
2017-08-14 04:03:12 +0000

    Finished learning conditionals/loops in EJS

--- c9-section/MoreExpress/EJSDemo/app.js
+++ c9-section/MoreExpress/EJSDemo/app.js
@@ -9,6 +9,15 @@ app.get("/fallinlovewith/:thing",function(req, res){
     res.render("love.ejs",{thingVar : req.params.thing});
 });
 
+app.get("/posts",function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Neil"},
+        {title: "My adorable pet bunny", author: "Susy"},
+        {title: "Can you believe this pomsky?", author: "Colt"},
+    ];
+    res.render("posts.ejs", {posts: posts});
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(req, res){
     console.log("Server running on PORT " + process.env.PORT);
