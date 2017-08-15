3a6275ed11cf6531f8fb6f2eb46294fc26ef3ec1
Neil Opena <neil1114o@gmail.com>
2017-08-15 18:13:59 +0000

    Finished Post Request

--- c9-section/MoreExpress/PostRequestDemo/app.js
+++ c9-section/MoreExpress/PostRequestDemo/app.js
@@ -1,12 +1,25 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 app.get("/",function(req, res){
     res.render("home");
 })
+
+app.get("/friends", function(req, res){
+    res.render("friends",{friends : friends});
+});
+
+app.post("/addfriend",function (req, res){
+    friends.push(req.body.newfriend);
+    res.redirect("/friends");
+});
 
 app.listen(process.env.PORT, process.env.IP,function(){
     console.log("Server running on PORT " + process.env.PORT);
--- c9-section/MoreExpress/readme.md
+++ c9-section/MoreExpress/readme.md
@@ -4,3 +4,18 @@
 * Explain what EJS is and why we use it
 * Pass variables to EJS templates
 
+#EJS Control Flow
+* Show examples of control flow in EJS templates
+* Write if statements in an EJS file
+* Write loops in an EJS file
+
+#Styles And Parials
+* Show how to properly include public assets
+* Properly configure our app to use EJS
+* Use partials to dry up our code!
+
+#Post Requests!!!
+* Write post routes, and test them with Postman
+* Use a form to send a post request
+* Use body parser to get form data
+
