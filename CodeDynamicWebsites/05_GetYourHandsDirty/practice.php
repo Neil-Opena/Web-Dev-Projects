9f9a72471a42157d9a046101f15a4965643abe4b
Neil Opena <neil1114o@gmail.com>
2017-08-20 13:30:59 -0400

    finished learning php basics

--- CodeDynamicWebsites/05_GetYourHandsDirty/practice.php
+++ CodeDynamicWebsites/05_GetYourHandsDirty/practice.php
@@ -1,24 +1,29 @@
 <?php
-	
+
 	// Define a Constant
-	
-	
+	define("TITLE","Variables and Constants");
+
+
 	// Your Variables
-	
-	
+	$name = "Neil";
+	$color = "Blue";
+	$year = 1997;
+
 	/*
 	Use PHP to calculate the difference
 	between your birth year and this year
 	to show your age dynamically
 	*/
 
-	
+	$age = 2017 - $year;
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Get Your Hands Dirty: <!-- PAGE TITLE --></title>
+		<title>Get Your Hands Dirty: <?php echo TITLE?><!-- PAGE TITLE --></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -26,33 +31,33 @@
 			<a href="/" title="Back to directory" id="logo">
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
-			
-			<h1>Get Your Hands Dirty: <small><!-- PAGE TITLE --></small></h1>
+
+			<h1>Get Your Hands Dirty: <small><?php echo TITLE?><!-- PAGE TITLE --></small></h1>
 			<hr>
-			
+
 			<h2>Your Example</h2>
-			
+
 			<div class="sandbox">
 				<h3>Today's Date:</h3>
-				<p><!-- DATE --></p>
-				
+				<p><?php echo date("F j, Y")?><!-- DATE --></p>
+
 				<h3>My Name:</h3>
-				<p><!-- YOUR NAME --></p>
-				
+				<p><?php echo $name?><!-- YOUR NAME --></p>
+
 				<h3>My Favourite Colour:</h3>
-				<p><!-- FAVOURITE COLOUR --></p>
-				
+				<p><?php echo $color?><!-- FAVOURITE COLOUR --></p>
+
 				<h3>My Age:</h3>
-				<p><!-- YOUR AGE --></p>
+				<p><?php echo $age?><!-- YOUR AGE --></p>
 			</div><!-- end sandbox -->
-			
+
 			<a href="index.php" class="button">Back to the final example</a>
-			
+
 			<hr>
-			
+
 			<small>&copy;<!-- THIS YEAR --> - <!-- YOUR NAME --></small>
 		</div><!-- end wrapper -->
-		
+
 		<div class="copyright-info">
 			<?php include('../assets/includes/copyright.php'); ?>
 		</div><!-- end copyright-info -->
